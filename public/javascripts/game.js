386a59782eff797707f555c9d018ffc2dd301559
Michael Rockway <michaelrockway@macbookpro.lan>
2025-01-08 09:35:04 -0700

    create game clue category from view

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,7 +1,58 @@
-$(function() {
+$(function () {
   $("#newGameForm").submit(function (e) {
     e.preventDefault();
     var formData = $("#newGameForm").serializeArray();
-    console.log(formData)
+    var obj = createGameObj(formData);
+    console.log(obj)
+
+    $.ajax({
+      type: "POST",
+      url: "/games/saveGame",
+      data: JSON.stringify({ data: obj }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+    }).done(function( data ) {console.log('data')});
+
   });
 });
+
+function createGameObj(formData) {
+  let obj = mapArrToJSON(formData);
+  let game = {}
+  game.gameName = obj.gameName
+  game['categories'] = mapCategories(obj)
+  return game;
+}
+
+function mapArrToJSON(form) {
+  let obj = {};
+  form.map((el) => obj[el["name"]] = el["value"]);
+  return obj;
+}
+
+function mapCategories(obj) {
+  let categories = []
+  for (const [k, v] of Object.entries(obj)) {
+    if (/(category)\d{1}(Name)/.test(k)) {
+      let catNum = parseInt(k.match(/\d+/)[0])
+      let clues = mapClues(obj, catNum)
+      categories.push({
+        name: v,
+        clues: clues
+      })
+    }
+  }
+  return categories
+}
+
+function mapClues(obj, catNum) {
+  let clues = []
+  for (let i = 1; i < 6; i++) {
+    clues.push({
+      question: obj[`cat${catNum}clueQ${i}`],
+      answer: obj[`cat${catNum}clueA${i}`],
+      value: obj[`cat${catNum}clueV${i}`]
+    })
+  }
+  return clues
+}
--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {Game, Clue, Category} = require('../models/index.js')
 
-/* GET home page. */
 router.get('/new', function(req, res, next) {
   res.render('newGame', { title: 'Jeopardy!', renderTime: new Date() });
 });
+
+
+router.get('/show', function(req, res, next) {
+  let games = Game.find()
+  res.render('newGame', { title: 'Jeopardy!', renderTime: new Date() });
+});
+
+
+router.post('/saveGame', async function(req,res,next) {
+  let gameReq = req.body.data;
+  let newGame = new Game({name: gameReq.gameName});
+  await newGame.save();
+
+  let categoryIds = []
+  for (let i = 0; i < gameReq.categories.length; i++) {
+    let cat = gameReq.categories[i];
+    let newCategory = new Category({
+      name: cat.name,
+      game: newGame.id
+    });
+    await newCategory.save();
+    categoryIds.push(newCategory.id);
+
+    let clueIds = []
+    for (let j = 0; j < cat.clues.length; j++) {
+      let clue = cat.clues[j];
+      let newClue = new Clue({
+        game: newGame.id,
+        category: newCategory.id,
+        question: clue.question,
+        answer: clue.answer,
+        value: clue.value
+      });
+      await newClue.save();
+      clueIds.push(newClue.id);
+    }
+    newCategory.clues = clueIds;
+    await newCategory.save();
+  }
+  newGame.categories = categoryIds;
+  await newGame.save();
+  res.status(200).send(newGame);
+})
 
 module.exports = router;
