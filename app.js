263c118355fdcf1dfbaec546f62ebd48ead7ab63
Michael Rockway <mrockway@gmail.com>
2025-01-19 17:44:51 -0700

    remove sync

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
 
-// const db = require("./models");
-// db.sequelize.sync()
-//   .then(() => {
-//     console.log("Synced db.");
-//   })
-//   .catch((err) => {
-//     console.log("Failed to sync db: " + err.message);
-//   });
-
 // Router setup
 var indexRouter = require('./routes/index');
 var gameRouter = require('./routes/game');
